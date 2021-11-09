92ddb9742a169e6a1b7156b53d873b9c34e42f26
sachinb2010 <sachinbhandari548@gmail.com>
2021-11-10 00:07:17 +0530

    ajax api

---
+++ script.js
@@ -0,0 +1,43 @@
+let fromsearch=document.querySelector(".search")
+let input=document.querySelector(".input")
+let newslist=document.querySelector(".class-list")
+errors=document.querySelector(".error")
+resets=document.querySelector(".reset")
+fromsearch.addEventListener("submit",retrive)
+function retrive(e){
+    if(input.value ==''){
+       errors.style.visibility ="visible"
+    //    alert("Cannot  be empty")
+      
+    }
+    else{
+        errors.style.visibility="hidden"
+    }
+   e.preventDefault()
+    const apikey="94f42a355dac46058db498796915b083"
+    let topic=input.value
+    
+
+    let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${apikey}`
+    fetch(url).then((res)=>{
+        return res.json()
+
+    }).then((data)=>{
+        console.log(data);
+        data.articles.forEach(article => {
+            let li=document.createElement('li')
+            let a= document.createElement('a')
+            a.setAttribute('href',article.url)
+            a.setAttribute('target','_blank')
+            a.textContent = article.title;
+            li.appendChild(a);
+            
+            newslist.appendChild(li);
+            
+        });
+    })
+   resets.addEventListener('click',()=>{
+       console.log("This is awsome and good");
+      newslist.reset();
+   })
+}
